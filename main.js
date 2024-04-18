e25d7664926dd85b20301c3dee7aca5fb92eaf6d
Mouhamad_Kraytem <moohamadkraytem15@gmail.com>
2024-04-18 19:04:57 +0300

    last edit

--- main.js
+++ main.js
@@ -3,43 +3,99 @@ let inp = document.getElementById("date");
 let but = document.querySelector("button");
 
 but.addEventListener("click", () => {
-  let bDate = new Date(inp.value);
+  //get birth date from user
+  let birthDate = new Date(inp.value);
 
-  let d1 = bDate.getDay();
-  let m1 = bDate.getMonth();
-  let y1 = bDate.getFullYear();
+  let birthDay = birthDate.getDate();
+  let birthMonth = birthDate.getMonth() + 1;
+  let birthYear = birthDate.getFullYear();
 
+  // initialize the current date
+  let currentDate = new Date();
 
-  let today = new Date();
+  let currentDay = currentDate.getDate();
+  let currentMonth = currentDate.getMonth() + 1;
+  let currentYear = currentDate.getFullYear();
 
-  let d2 = today.getDay();
-  let m2 = today.getMonth();
-  let y2 = today.getFullYear();
+  let resDay , resMonth , resYear;
 
-  let d3,m3,y3;
-  y3 = y2 - y1;
+  resYear = currentYear - birthYear;
 
-  if (m2 >= m1) {
-    m3 = m2 - m1;
+  if (currentMonth >= birthMonth) {
+    resMonth = currentMonth - birthMonth;
   } else {
-    y3--;
-    m3 = 12 + m2 - m1;
+    resYear--;
+    resMonth = 12 + currentMonth - birthMonth;
   }
 
-  if (d2 >= d1) {
-    d3 = d2 -d1;
+  if (currentDay >= birthDay) {
+    resDay = currentDay - birthDay;
   } else {
-    m3--;
-    d3 = getDayInMonth(y1 , m1) + d2 - d1;
+    resMonth--;
+    resDay = getDateInMonth(birthYear, birthMonth) + currentDay - birthDay;
   }
 
-  if (m3 < 0) {
-    m3 = 11;
-    y3 --;
+  if (resMonth < 0) {
+    resMonth = 11;
+    resYear--;
   }
-  console.log(y3 , m3 , d3);
+
+  //print result with handle null values logique error
+  let days , months , years;
+  let flag = 0;
+  let result = "you have ";
+
+  if (resYear > 0) {
+    let y = "year"
+    if (resYear > 1) {
+      y += "s";
+    }
+    years = `<span>${resYear} ${y} </span> `;
+    result += years;
+    flag = 1;
+  }
+
+  if (resMonth > 0) {
+    let m = "month"
+    if (resMonth > 1) {
+      m += "s";
+    }
+    if (flag === 1) {
+      months = ` and <span> ${resMonth} ${m} </span>`;
+      result += months;
+    }
+    else
+    {
+      months = ` <span> ${resMonth} ${m} </span>`;
+      result += months;
+    }
+    flag = 1
+  }
+
+  if (resDay > 0) {
+    let d = "day"
+    if (resDay > 1) {
+      d += "s";
+    }
+    if (flag === 1) {
+      days = `and <span> ${resDay} ${d} </span>`;
+      result += days;
+    }else{
+      days = `<span> ${resDay} ${d} </span> `;
+      result += days;
+    }
+    
+  }
+  if (resDay > 0 || resMonth > 0 || resYear) {
+    document.getElementById("result").innerHTML = result;
+    // console.log(currentDate , currentDay , currentMonth ,  currentYear);
+    // console.log(resDay , resMonth , resYear);
+  }
 })
 
-function getDayInMonth(year ,month) {
+function getDateInMonth(year , month) {
+  console.log(new Date(year, month, 0).getDate);
   return new Date(year , month , 0).getDate();
 }
+
+
